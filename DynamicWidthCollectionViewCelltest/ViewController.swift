be78b4b3a5fba9ebcd074fd58fc3517c66d6908b
Saw Pyae Htun <sawpyaehtun@gmail.com>
2020-11-05 14:59:51 +0630

    Initial Commit

---
+++ DynamicWidthCollectionViewCelltest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicWidthCollectionViewCelltest
+//
+//  Created by saw pyaehtun on 05/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
